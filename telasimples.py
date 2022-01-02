4b87530eaab32d88c847c226d560ff2356d17e77
LazaroLuz <lazaro.luz@hotmail.com>
2022-01-02 19:29:33 -0300

    criação de um relogio.

--- telasimples.py
+++ telasimples.py
@@ -1,14 +1,19 @@
 import PySimpleGUI as sg
+import datetime
 
 
-layout = [[sg.T('Bom Dia')],
-          [sg.T('Teste do Dia')]
+curr_dt_time = datetime.datetime.now()
+horas = curr_dt_time.strftime("%H:%M:%S")
+layout = [[sg.T(horas, text_color='blue', border_width=2, font='Arial 25', k='_H_')]
           ]
 
 janela = sg.Window('teste do github', layout)
 
 while True:
-    event, value = janela.read()
+    event, value = janela.read(timeout=10)
+    curr_dt_time = datetime.datetime.now()
+    h_c = curr_dt_time.strftime("%H:%M:%S")
     if event == sg.WIN_CLOSED:
         break
+    janela['_H_'].update(h_c)
 janela.close()
